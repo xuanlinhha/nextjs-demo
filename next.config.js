fb5b0b6e89c29c219d4eed2c58baf81fb2bc9b6e
Xuan Linh Ha <xuanlinhha@Linh-M1Pro.local>
2022-12-07 21:38:11 +0700

    add image optimization

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const withExportImages = require('next-export-optimize-images')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = withExportImages({nextConfig})
